43166ce9512fe472ef3108f192b82e8435b9c199
bk <withbyeongk@gmail.com>
2024-01-12 10:46:49 +0900

    fix : duplicated class name

--- rms/src/main/java/com/iron/rms/reservation/service/ReservationServiceImpl.java
+++ rms/src/main/java/com/iron/rms/reservation/service/ReservationServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class PartyServiceImpl {
+public class ReservationServiceImpl {
 
     private final ReservationRepository reservationRepository;
 
