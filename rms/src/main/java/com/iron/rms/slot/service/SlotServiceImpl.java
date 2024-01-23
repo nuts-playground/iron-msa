d339cce218ae454b6a50890e76bf56115862cfdd
withbyeongk <withbyeongk@gmail.com>
2024-01-23 23:55:29 +0900

    test : commit test

--- rms/src/main/java/com/iron/rms/slot/service/SlotServiceImpl.java
+++ rms/src/main/java/com/iron/rms/slot/service/SlotServiceImpl.java
@@ -1,8 +1,12 @@
 package com.iron.rms.slot.service;
 
+import com.iron.rms.slot.repository.SlotRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SlotServiceImpl implements SlotService {
+	private final SlotRepository slotRepository;
 
 }
